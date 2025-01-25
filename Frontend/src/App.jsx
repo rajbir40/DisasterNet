445f1cb464b7b9ce4070288016bdb33359f95cb8
Jaisman <jaismanjhinger@gmail.com>
2025-01-25 09:59:25 +0530

    login,signup,profile

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import CarbonCalculator from './User Tools/carbonCalculator'
+import Login from './User Tools/Login/Login';
+import Signup from './User Tools/Signup';
+import UserProfile from './User Tools/Profile';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -6,7 +8,9 @@ function App() {
   return (
     <Router>
      <Routes>
-      <Route path="/" element={<CarbonCalculator />} />
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<UserProfile/>}/>
      </Routes>
     </Router>
   )
